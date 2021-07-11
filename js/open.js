14faa15b57c9a470881d16ea60b319d2c359ea62
Siddhesh Kamble <siddhesh.kamble7867@gmail.com>
2021-07-11 12:22:59 +0530

    update v1

--- js/open.js
+++ js/open.js
@@ -2,11 +2,13 @@ const icon = document.querySelector('.chat_icon');
 const com_icon = document.querySelector('.fa-comments');
 const close_icon = document.querySelector('.fa-times');
 const frame = document.querySelector('.chat_frame');
+const iris = document.querySelector('.iris');
 
 if (icon) {
   icon.addEventListener('click', () => {
     frame.classList.toggle('active');
     com_icon.classList.toggle('active');
     close_icon.classList.toggle('active');
+    iris.classList.toggle('active');
   });
 }
